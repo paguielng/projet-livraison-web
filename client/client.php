05715df6c4d9daf1f110cd637da895c55bad67da
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-02-26 21:10:08 +0100

    Update client.php

--- client/client.php
+++ client/client.php
@@ -1,1 +1,13 @@
-
+<form action="gestion-client.php" method="post">
+    <label for="client">Sélectionnez un client :</label>
+    <select name="id_client">
+        <?php
+        include("../includes/db.php");
+        $result = $pdo->query("SELECT * FROM clients");
+        while ($row = $result->fetch()) {
+            echo "<option value='{$row['id']}'>{$row['nom']}</option>";
+        }
+        ?>
+    </select>
+    <button type="submit">Valider</button>
+</form>
