9f040cb4504f05e0eaf35f68803b9a872b724dd6
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-02-26 21:10:50 +0100

    Update depot-client.php

--- client/depot-client.php
+++ client/depot-client.php
@@ -1,1 +1,8 @@
+<?php
+include("../includes/db.php");
+$id_client = $_POST['id_client'];
+$id_destinataire = $_POST['id_destinataire'];
 
+$pdo->exec("INSERT INTO colis (id_client, id_livreur, statut) VALUES ($id_client, NULL, 'En attente')");
+echo "Colis déposé avec succès.";
+?>
