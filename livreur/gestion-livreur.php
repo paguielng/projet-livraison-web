b98902d274d89963e3ab2988fae978f06a7fd371
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-02-26 21:09:34 +0100

    Update gestion-livreur.php

--- livreur/gestion-livreur.php
+++ livreur/gestion-livreur.php
@@ -1,1 +1,19 @@
+<?php
+include("../includes/db.php");
+$id_livreur = $_POST['id_livreur'];
 
+// Affichage des colis du livreur
+$colis = $pdo->query("SELECT * FROM colis WHERE id_livreur = $id_livreur");
+while ($row = $colis->fetch()) {
+    echo "<p>Colis #{$row['id']} - Statut : {$row['statut']}</p>";
+    echo "<form action='statut-livreur.php' method='post'>
+            <input type='hidden' name='id_colis' value='{$row['id']}'>
+            <select name='statut'>
+                <option>En attente</option>
+                <option>En cours de livraison</option>
+                <option>Livré</option>
+            </select>
+            <button type='submit'>Modifier</button>
+          </form>";
+}
+?>
