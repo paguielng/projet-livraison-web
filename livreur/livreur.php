3088286b6fdb57f5c478bd7def44ff571a62d0a3
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-02-26 21:09:12 +0100

    Update livreur.php

--- livreur/livreur.php
+++ livreur/livreur.php
@@ -1,1 +1,13 @@
-
+<?php include("../includes/db.php"); ?>
+<form action="gestion-livreur.php" method="post">
+    <label for="livreur">Sélectionnez un livreur :</label>
+    <select name="id_livreur">
+        <?php
+        $result = $pdo->query("SELECT * FROM livreurs");
+        while ($row = $result->fetch()) {
+            echo "<option value='{$row['id']}'>{$row['nom']}</option>";
+        }
+        ?>
+    </select>
+    <button type="submit">Valider</button>
+</form>
